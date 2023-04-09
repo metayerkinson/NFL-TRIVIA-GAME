e5dc3f4662fa9a87700297b9388d9a45ec41f777
Your Name <metayerkinson@gmail.com>
2023-04-08 21:04:31 -0400

    added q&A

---
+++ game.js
@@ -0,0 +1,26 @@
+
+
+// adding my Questions//
+
+const Gamequestions = [
+    {
+      question: "What year did the miami Dolphins go underdeafted and went 17-0? ",
+      answer: "1970"
+    },
+    {
+      question: "How many teams play in the NFL?",
+      answer: "32 Teams"
+    },
+    {
+      question: "How many head coaches are there in the NFL?",
+      answer: "32"
+    },
+    {
+      question: " How many Superbowls have Miami Dolphins win?",
+      answer: "2"
+    },
+    {
+      question: "How many Superbowls have the Bufalo Bills win?",
+      answer: "0"
+    }
+  ];
